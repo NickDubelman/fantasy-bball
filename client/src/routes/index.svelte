a66b0c7426200d2214b7f4b6829552cc8f610109
Nick Dubelman <ndubelman@arista.com>
2021-03-17 23:19:55 -0700

    bootstrap basic frontend

---
+++ client/src/routes/index.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+  <title>Fantasy Basketball</title>
+</svelte:head>
+
+<h1>Fantasy Basketball</h1>
+
+<style>
+  a {
+    background-color: #542581;
+    color: white;
+    padding: 1em 1.4em;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+</style>
