4d9556ccd53b6ed0b66bb98f9230f3525061128e
Nick Dubelman <ndubelman@gmail.com>
2021-03-30 23:32:36 -0700

    add button to create a league

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -2,10 +2,17 @@
   <title>Fantasy Basketball</title>
 </svelte:head>
 
-<h1>Fantasy Basketball!</h1>
+<h1>Your leagues</h1>
+
+<button type="button">Create a league</button>
 
 <style lang="postcss">
   h1 {
     @apply text-4xl font-serif;
+  }
+
+  button {
+    @apply bg-blue-500 hover:bg-blue-600 hover:shadow-lg focus:outline-none;
+    @apply text-white text-sm mt-3 py-2.5 px-5 rounded-md;
   }
 </style>
